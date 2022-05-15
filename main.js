3a0858dcd1fbc1974ee4a1338dfc6d31befbe910
AlekseyVolovik <103026773+AlekseyVolovik@users.noreply.github.com>
2022-05-15 12:37:42 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,4 +4,5 @@ const mobileNav = document.querySelector(".mobile-nav");
 navBtn.addEventListener("click", function() {
 	mobileNav.classList.toggle("mobile-nav-active");
 	navBtn.classList.toggle("nav-button-close");
-})
+});
+
